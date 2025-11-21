732aec1c18a166aea4a7d01d6df5a92ed930d713
Pau López Millan <paulopez@iesmontsia.org>
2025-11-21 16:17:18 +0100

    Dia 2.2

--- jocs.py
+++ jocs.py
@@ -1,35 +1,88 @@
 from robot import robot
 import random
+from time import sleep
 def janken():
     opcions = ["pedra", "paper", "tisores"]
     while True:
-        eleccio = input("Escull pedra, paper o tisores (S/s per sortir): ")
-        eleccio_maquina = robot().playing()
-        if eleccio == "S" or eleccio == "s":
+        dificultat = input("vols jugar al millor de 3 rondes o al millor de 5? (S/s per sortir) ")
+        if dificultat == "S" or dificultat == "s":
             print("Fins la propera!")
             break
-        elif eleccio not in opcions:
-            print("Elecció no vàlida. Torna-ho a intentar.")
-        elif eleccio == eleccio_maquina:
-            print("Empat")
-        elif (eleccio == "pedra" and eleccio_maquina == "tisores"):
-            print("Felicitats, has guanyat!")
-            break
-        elif (eleccio == "paper" and eleccio_maquina == "pedra"):
-            print("Felicitats, has guanyat!")
-            break
-        elif (eleccio == "tisores" and eleccio_maquina == "paper"):
-            print("Felicitats, has guanyat!")
-            break
-        elif (eleccio == "pedra" and eleccio_maquina == "paper"):
-            print("Has perdut! La màquina ha escollit", eleccio_maquina)
-            break
-        elif (eleccio == "paper" and eleccio_maquina == "tisores"):
-            print("Has perdut! La màquina ha escollit", eleccio_maquina)
-            break
-        elif (eleccio == "tisores" and eleccio_maquina == "pedra"):
-            print("Has perdut! La màquina ha escollit", eleccio_maquina)
-            break
+        rondes_usuari = 0
+        rondes_maquina = 0
+        if dificultat not in ["3","5"]:
+            print("opció no vàlida, torna-ho a intentar")
+            continue
+        elif dificultat == "3":
+            while rondes_usuari < 4 and rondes_maquina < 4:
+                eleccio = input("Escull pedra, paper o tisores (S/s per sortir): ")
+                eleccio_maquina = robot().playing()
+                if eleccio not in opcions:
+                    print("Elecció no vàlida. Torna-ho a intentar.")
+                elif eleccio == eleccio_maquina:
+                    print("Empat")
+                elif (eleccio == "pedra" and eleccio_maquina == "tisores"):
+                    print("Felicitats, has guanyat!")
+                    rondes_usuari += 1
+                elif (eleccio == "paper" and eleccio_maquina == "pedra"):
+                    print("Felicitats, has guanyat!")
+                    rondes_usuari += 1
+                elif (eleccio == "tisores" and eleccio_maquina == "paper"):
+                    print("Felicitats, has guanyat!")
+                    rondes_usuari += 1
+                elif (eleccio == "pedra" and eleccio_maquina == "paper"):
+                    print("Has perdut! La màquina ha escollit", eleccio_maquina)
+                    rondes_maquina += 1
+                elif (eleccio == "paper" and eleccio_maquina == "tisores"):
+                    print("Has perdut! La màquina ha escollit", eleccio_maquina)
+                    rondes_maquina += 1
+                elif (eleccio == "tisores" and eleccio_maquina == "pedra"):
+                    print("Has perdut! La màquina ha escollit", eleccio_maquina)
+                    rondes_maquina += 1
+                elif eleccio == "S" or eleccio == "s":
+                    print("Fins la propera!")
+                    break
+                elif rondes_usuari == 3:
+                    print("Enhorabona! Has guanyat la partida!")
+                    break
+                elif rondes_maquina == 3:
+                    print("La màquina ha guanyat la partida. Millor sort la pròxima vegada!")
+                    break
+        elif dificultat == "5":
+            while rondes_usuari < 6 and rondes_maquina < 6:
+                eleccio = input("Escull pedra, paper o tisores (S/s per sortir): ")
+                eleccio_maquina = robot().playing()
+                if eleccio not in opcions:
+                    print("Elecció no vàlida. Torna-ho a intentar.")
+                elif eleccio == eleccio_maquina:
+                    print("Empat")
+                elif (eleccio == "pedra" and eleccio_maquina == "tisores"):
+                    print("Felicitats, has guanyat!")
+                    rondes_usuari += 1
+                elif (eleccio == "paper" and eleccio_maquina == "pedra"):
+                    print("Felicitats, has guanyat!")
+                    rondes_usuari += 1
+                elif (eleccio == "tisores" and eleccio_maquina == "paper"):
+                    print("Felicitats, has guanyat!")
+                    rondes_usuari += 1
+                elif (eleccio == "pedra" and eleccio_maquina == "paper"):
+                    print("Has perdut! La màquina ha escollit", eleccio_maquina)
+                    rondes_maquina += 1
+                elif (eleccio == "paper" and eleccio_maquina == "tisores"):
+                    print("Has perdut! La màquina ha escollit", eleccio_maquina)
+                    rondes_maquina += 1
+                elif (eleccio == "tisores" and eleccio_maquina == "pedra"):
+                    print("Has perdut! La màquina ha escollit", eleccio_maquina)
+                    rondes_maquina += 1
+                elif eleccio == "S" or eleccio == "s":
+                    print("Fins la propera!")
+                    break
+                elif rondes_usuari == 5:
+                    print("Enhorabona! Has guanyat la partida!")
+                    break
+                elif rondes_maquina == 5:
+                    print("La màquina ha guanyat la partida. Millor sort la pròxima vegada!")
+                    break
 
 
 def nana():
